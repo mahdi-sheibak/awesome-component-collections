d9802d40aa01bf6ac65627ced9fc2fbb73ad0eac
Mahdi Zoraghi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2025-04-11 07:38:02 -0400

    refactor template

--- script.ts
+++ script.ts
@@ -26,9 +26,7 @@ for (const item of list) {
   finalContent += `
 <!-- ${item.name} -->
    <section>
-      ${
-        item.repo
-          ? `<a href="${item.repo}">
+      <a href="${item.repo || item.website}">
          ${
            item.logoURL
              ? `<img
@@ -39,9 +37,7 @@ for (const item of list) {
           />`
              : "LOGO"
          }
-      </a>`
-          : "<span/>"
-      }
+      </a>
       <div>
          <span>${item.longName} ${
     item.repo ? `[<a href="${item.repo}">repo</a>]` : ""
